81a03ccf71ead1393e6c210f396486c3cd673529
Alberth Ruado <alberth@networkmonks.net>
2024-08-30 18:38:22 +0800

    [Modified] Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="main-wrapper">
       <NavBar />
+      <Main />
+      <Footer />
     </div>
   );
 }
